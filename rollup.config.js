b160fcb6eb9d9032e50ff2aebf321ab6d9898d07
thomas <thomas.chiantia@gmail.com>
2021-03-10 17:09:16 -0500

    added fit addon, fixed main export for xterm

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,31 @@
 const resolve = require("@rollup/plugin-node-resolve").nodeResolve;
 const alias = require("@rollup/plugin-alias");
 const path = require("path");
-export default {
-  input: path.join(__dirname, "out-es6", "src", "browser", "Terminal.js"),
-  output: {
-    file: "./dist/es6/xterm.js",
-    format: "esm", // umd
-    sourcemap: true,
+export default [
+  {
+    input: path.join(__dirname, "out-es6/src/browser/public/Terminal.js"),
+    output: {
+      file: "./dist/es6/xterm.js",
+      format: "esm", // umd
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      alias({
+        entries: [
+          { find: /^common\/(.*)/, replacement: "./out-es6/src/common/$1.js" },
+          { find: /^browser\/(.*)/, replacement: "./out-es6/src/browser/$1.js" },
+        ],
+      }),
+    ],
   },
-  plugins: [
-    resolve(),
-    alias({
-      entries: [
-        { find: /^common\/(.*)/, replacement: "./out-es6/src/common/$1.js" },
-        { find: /^browser\/(.*)/, replacement: "./out-es6/src/browser/$1.js" },
-      ],
-    }),
-  ],
-};
+  {
+    input: path.join(__dirname, "out-es6/addons/xterm-addon-fit/src/FitAddon.js"),
+    output: {
+      file: "./dist/es6/addons/FitAddon.js",
+      format: "esm", // umd
+      sourcemap: true,
+    },
+    plugins: [resolve()],
+  },
+];
